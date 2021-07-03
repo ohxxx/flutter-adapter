2ad216d883ab5057fbf24f9a674ac2eec4ef4fa3
humengming <humengmingxx@gmail.com>
2021-07-03 13:48:01 +0800

    feat(package)：add google_fonts & change layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_adapt/adapt.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,62 +27,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  /// [主标题组件]
+  /// card小组件
   ///
   /// [title] 当前标题的文本信息，String类型
-  Text _title(String title) {
-    return Text(
-      title,
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: Adapt().adaptSp(16),
-      ),
+  Container _customCard(String title) {
+    return Container(
+      width: Adapt().adaptPx(375),
+      height: Adapt().adaptPx(50),
+      margin: EdgeInsets.only(top: Adapt().adaptPx(10)),
+      color: Color(0xff3C5186),
+      child: _customText(title),
     );
   }
 
-  /// [副标题组件]
+  /// text组件
   ///
   /// [title] 当前标题的文本信息，String类型
-  Text _subTitle(String title) {
-    return Text(
-      title,
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: Adapt().adaptSp(10),
+  Align _customText(String title) {
+    return Align(
+      child: Text(
+        title,
+        style: GoogleFonts.goldman(
+          color: Colors.white,
+          fontSize: Adapt().adaptSp(16),
+        ),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    /// [初始化]
+    /// Adapt 初始化
     ///
     /// 上下文传入进行媒体查询
     Adapt.init(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Adapt'),
+        title: Text(
+          'Flutter Adapt',
+          style: GoogleFonts.goldman(),
+        ),
+        backgroundColor: Color(0xff3C5186),
       ),
       body: Center(
         child: Column(
           children: <Widget>[
-            Container(
-              width: Adapt().adaptPx(200),
-              height: Adapt().adaptPx(200),
-              margin: EdgeInsets.only(top: Adapt().adaptPm(30)),
-              color: Colors.blue,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _title('Adapt尺寸使用介绍：'),
-                  _subTitle('布局的宽高使用：Adapt().adaptPx(x)，根据的是屏幕的宽度比'),
-                  _subTitle('布局的上下边距使用：Adapt().adaptPm(x)，根据的是屏幕的高度比'),
-                  _subTitle('字体使用：Adapt().adaptSp(x)'),
-                  // todo: add more example
-                ],
-              ),
-            )
+            _customCard('Liquid layout'),
+            _customCard('Full screen layout'),
           ],
         ),
       ),
